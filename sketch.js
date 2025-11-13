3aaf9b635b593c0174af260ba7ec3e4bd92a871b
LaSpadaIlaria <ilalaspada@gmail.com>
2025-11-13 13:28:52 +0100

    preparazione

--- sketch.js
+++ sketch.js
@@ -1,15 +1,20 @@
+let data;
+
 function preload() {
-  // put preload code here
+  data = loadTable("assets/vulcanidata.csv", "csv", "header");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
-  const message =
-    "This is a template repository\nfor the course Laboratorio di Computergrafica\nCommunication Design, Politecnico di Milano";
-  textAlign(CENTER, CENTER);
-  textSize(16);
-  text(message, width / 2, height / 2);
+  
+  /*if (data) {
+    console.log("Dati caricati correttamente");
+    console.log(data.getRowCount() + " righe in tabella");
+    console.log(data.getColumnCount() + " colonne in tabella");
+    console.log("Esempio:", data.getRow(0).arr);
+  } else {
+    console.log("Errore: tabella non caricata");
+  } */
 }
 
 function draw() {
